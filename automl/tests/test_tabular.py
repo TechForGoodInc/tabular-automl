6c55de5204772f8cedfbf23ce3f10cd34dc69eac
Harison Gachuru <harisongachuru@gmail.com>
2021-07-30 00:49:16 +0300

    Updated setUp method in tabular automl tests

--- automl/tests/test_tabular.py
+++ automl/tests/test_tabular.py
@@ -16,6 +16,8 @@ class TabularAutoMLTestCase(unittest.TestCase):
         self.target_col = "Survived"
         self.task_type = "classification"
 
+        print(f"DATA_DIR: {DATA_DIR}")
+
     def test_basic(self):
         with self.assertRaises(TypeError):
             TabularAutoML()
