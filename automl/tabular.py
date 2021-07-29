5bd3c3f05d98a7cb1e1c668fd3d62c5d661d9f94
Harison Gachuru <harisongachuru@gmail.com>
2021-07-30 01:09:26 +0300

    Fix bug in TabularAutoML.get_data

--- automl/tabular.py
+++ automl/tabular.py
@@ -52,7 +52,7 @@ class TabularAutoML:
         if index_col is not None:
             try:
                 index = list(set(data.columns) & {index_col})
-                data.set_index(index)
+                data.set_index(index[0])
             except KeyError:
                 if index_col is not None:
                     raise ValueError(f"{index_col} not found in data")
