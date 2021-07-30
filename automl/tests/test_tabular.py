b142b45f4a2a2765a400d773d5ddba7b1a7e5b0d
Harison Gachuru <harisongachuru@gmail.com>
2021-07-30 23:22:49 +0300

    Update TabularAutoML tests

--- automl/tests/test_tabular.py
+++ automl/tests/test_tabular.py
@@ -11,11 +11,11 @@ from automl.tabular import TabularAutoML
 class TabularAutoMLTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.data_dir = DATA_DIR / "titanic"
-        cls.train_data_dir = cls.data_dir / "train.csv"
-        cls.index = "PassengerId"
-        cls.target = "Survived"
-        cls.task = "classification"
+        cls.data_path = DATA_DIR / "titanic"
+        cls.train_data_path = cls.data_path / "train.csv"
+        cls.index_col = "PassengerId"
+        cls.target_col = "Survived"
+        cls.task_type = "classification"
 
     def test_basic(self):
         with self.assertRaises(TypeError):
@@ -28,57 +28,57 @@ class TabularAutoMLTestCase(unittest.TestCase):
         
         # non-existent path
         with self.assertRaises(ValueError):
-            TabularAutoML(self.data_dir / "does_not_exist.csv")
+            TabularAutoML(self.data_path / "does_not_exist.csv")
 
         # unsupported format
         with self.assertRaises(ValueError):
-            TabularAutoML(self.data_dir / "titanic.zip")
+            TabularAutoML(self.data_path / "titanic.zip")
 
     def test_get_module(self):
         with self.assertRaises(ValueError):
             TabularAutoML(
-                self.train_data_dir,
-                target_col=self.target
+                self.train_data_path,
+                target_col=self.target_col
             )
 
         with self.assertRaises(ValueError):
             TabularAutoML(
-                self.train_data_dir,
-                target_col=self.target,
+                self.train_data_path,
+                target_col=self.target_col,
                 task_type="unknown"
             )
 
     def test_get_data(self):
         with self.assertRaises(ValueError):
             TabularAutoML(
-                self.train_data_dir,
+                self.train_data_path,
                 index_col="unknown",
-                target_col=self.target,
-                task_type=self.task
+                target_col=self.target_col,
+                task_type=self.task_type
             )
         
         with self.assertRaises(ValueError):
             TabularAutoML(
-                self.train_data_dir,
-                index_col=self.index,
+                self.train_data_path,
+                index_col=self.index_col,
                 target_col="unknown",
-                task_type=self.task
+                task_type=self.task_type
             )
 
         automl = TabularAutoML(
-            self.train_data_dir,
-            index_col=self.index,
-            target_col=self.target,
-            task_type=self.task
+            self.train_data_path,
+            index_col=self.index_col,
+            target_col=self.target_col,
+            task_type=self.task_type
         )
-        self.assertTrue(isinstance(automl.data, pd.DataFrame))
+        self.assertTrue(isinstance(automl.train_data, pd.DataFrame))
 
     def test_get_sample(self):
         automl = TabularAutoML(
-            self.train_data_dir,
-            index_col=self.index,
-            target_col=self.target,
-            task_type=self.task
+            self.train_data_path,
+            index_col=self.index_col,
+            target_col=self.target_col,
+            task_type=self.task_type
         )
         sample= automl._get_sample()
         self.assertEqual(sample.shape, (89,12))
@@ -91,10 +91,10 @@ class TabularAutoMLTestCase(unittest.TestCase):
 
     def test_get_best_model(self):
         automl = TabularAutoML(
-            self.train_data_dir,
-            index_col=self.index,
-            target_col=self.target,
-            task_type=self.task
+            self.train_data_path,
+            index_col=self.index_col,
+            target_col=self.target_col,
+            task_type=self.task_type
         )
         config = {"setup": dict(silent=True)}
         automl.get_best_model(config)
