9b66da8b2916d3d155795c13c1eda5dc67219704
Harison Gachuru <harisongachuru@gmail.com>
2021-08-23 12:32:39 +0300

    Migrate all settings to the settings module

--- tabular_automl/settings.py
+++ tabular_automl/settings.py
@@ -1,4 +1,17 @@
+"""
+Settings for the tabular_automl package
+"""
+
 from pathlib import Path
 
+import pandas as pd
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATA_DIR = BASE_DIR / "data"
+
+LARGE_DATASET_ROWS = int(1e5)
+
+SUPPORTED_TASK_TYPES = ("regression", "classification")
+
+FILE_READERS = {".csv": pd.read_csv}
