4c81d3d3c28ae948b66285e5273f2f1e809ad653
Harison Gachuru <harisongachuru@gmail.com>
2021-08-23 18:03:32 +0300

    Update settings and gitignore

--- tabular_automl/settings.py
+++ tabular_automl/settings.py
@@ -10,6 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = BASE_DIR / "data"
 
+MODEL_OUTPUTS_DIR = BASE_DIR / "model_outputs"
+
 LARGE_DATASET_ROWS = int(1e5)
 
 SUPPORTED_TASK_TYPES = ("regression", "classification")
