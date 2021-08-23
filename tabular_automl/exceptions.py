42fc438bf93765b58cd1794622ddb7b75366ef70
Harison Gachuru <harisongachuru@gmail.com>
2021-08-23 12:34:44 +0300

    Add custom exceptions

---
+++ tabular_automl/exceptions.py
@@ -0,0 +1,15 @@
+"""Custom exceptions for tabular automl"""
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class UnsupportedFileFormatError(Error):
+    """Raised when the file format is unsupported"""
+    pass
+
+
+class UnsupportedTaskTypeError(Error):
+    """Raised when the task type is unsupported"""
+    pass
