38a93a77c6609959a912914cced86a745e68bd78
Harison Gachuru <harisongachuru@gmail.com>
2021-08-26 23:00:49 +0300

    Remove unnecessary  statements

--- tabular_automl/exceptions.py
+++ tabular_automl/exceptions.py
@@ -4,16 +4,10 @@
 class Error(Exception):
     """Base class for other exceptions"""
 
-    pass
-
 
 class UnsupportedFileFormatError(Error):
     """Raised when the file format is unsupported"""
 
-    pass
-
 
 class UnsupportedTaskTypeError(Error):
     """Raised when the task type is unsupported"""
-
-    pass
